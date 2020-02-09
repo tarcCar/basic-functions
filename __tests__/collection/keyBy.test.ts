f6f0c964bc64f185d36493a90118285bea9d3cf3
Tarcísio <tarc.car@gmail.com>
2020-02-09 16:07:47 -0300

    key by

---
+++ __tests__/collection/keyBy.test.ts
@@ -0,0 +1,23 @@
+import { keyBy } from '../../src/collections';
+describe('Collection keyBy methdos', () => {
+  it('Should execute keyBy on array without key', () => {
+    const keysBys = keyBy(['a', 'b', 'c']);
+    expect(keysBys).toStrictEqual({ a: 'a', b: 'b', c: 'c' });
+  });
+
+  it('Should execute keyBy on array with key', () => {
+    const keysBys = keyBy(
+      [
+        { id: 'a1', title: 'abc' },
+        { id: 'b2', title: 'def' },
+      ],
+      'id',
+    );
+    expect(keysBys).toStrictEqual({ a1: { id: 'a1', title: 'abc' }, b2: { id: 'b2', title: 'def' } });
+  });
+
+  it('Should execute keyBy on object with key', () => {
+    const keysBys = keyBy({ data: { id: 'a1', title: 'abc' } }, 'id');
+    expect(keysBys).toStrictEqual({ a1: { id: 'a1', title: 'abc' } });
+  });
+});
--- src/collections.ts
+++ src/collections.ts
@@ -13,3 +13,20 @@ export function groupBy(array: any[], key: string) {
     return storage;
   }, {});
 }
+
+const keyByArray = (array: any[], key?: string) =>
+  (array || []).reduce((r, x) => ({ ...r, [key ? x[key] : x]: x }), {});
+/**
+ * Creates an object composed of keys generated from the results of running each element of collection through iteratee.
+ * @param array
+ * @param key
+ */
+export function keyBy(array: any[] | any, key?: string) {
+  const c = array || {};
+  return Array.isArray(c)
+    ? keyByArray(c, key)
+    : keyByArray(
+        Object.keys(c).map(key => c[key]),
+        key,
+      );
+}
