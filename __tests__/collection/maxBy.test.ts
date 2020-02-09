7bc4c2337bb7e1d9bc6327740f26e69e66c7743a
Tarcísio <tarc.car@gmail.com>
2020-02-09 16:30:23 -0300

    max by test

---
+++ __tests__/collection/maxBy.test.ts
@@ -0,0 +1,54 @@
+import { maxBy } from '../../src/collections';
+describe('Collection keyBy methdos', () => {
+  it('Should execute max string value from array with key', () => {
+    const maxValue = maxBy(
+      [
+        {
+          name: 'AA',
+        },
+        {
+          name: 'BB',
+        },
+        {
+          name: 'CC',
+        },
+      ],
+      'name',
+    );
+    expect(maxValue).toStrictEqual({ name: 'CC' });
+  });
+  it('Should execute max number value from array with key', () => {
+    const maxValue = maxBy(
+      [
+        {
+          value: 45,
+        },
+        {
+          value: 33,
+        },
+        {
+          value: 5069,
+        },
+      ],
+      'value',
+    );
+    expect(maxValue).toStrictEqual({ value: 5069 });
+  });
+  it('Should execute max date value from array with key', () => {
+    const maxValue = maxBy(
+      [
+        {
+          date: new Date(2020, 1, 23),
+        },
+        {
+          date: new Date(2020, 1, 21),
+        },
+        {
+          date: new Date(2020, 2, 25),
+        },
+      ],
+      'date',
+    );
+    expect(maxValue).toStrictEqual({ date: new Date(2020, 2, 25) });
+  });
+});
