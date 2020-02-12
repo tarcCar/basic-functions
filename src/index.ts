0e20ac8686436d4ccf4255df0f77f9af33b6eb58
Tarcísio <tarc.car@gmail.com>
2020-02-12 20:46:55 -0300

    export all methods from index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-import { chunck } from './array';
+export {
+  chunck,
+  compact,
+  flatten,
+  flattenDeep,
+  difference,
+  fromPairs,
+  intersection,
+  takeRight,
+  without,
+} from './array';
 
-export default { chunck };
+export { groupBy, keyBy, maxBy, minBy, sample, sortBy, uniq } from './collections';
+
+export { debounce, partial, throttle } from './functions';
+
+export { isEmpty } from './lang';
+
+export { inRange, random, randomInt } from './number';
+
+export { extend, get, omit, pick, pickBy } from './object';
+
+export { capitalize, onlyNumbers, upperFirst } from './string';
